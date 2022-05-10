19155e08e840a57976824daa5e0d16c72b7bac01
Paul Hendricks <paul@digitalhurricane.io>
2022-05-10 15:44:51 -0600

    remove component from entryComponents

--- projects/ngx-photo-editor/src/lib/ngx-photo-editor.module.ts
+++ projects/ngx-photo-editor/src/lib/ngx-photo-editor.module.ts
@@ -11,7 +11,7 @@ import { MatTooltipModule } from '@angular/material';
     MatTooltipModule,
   ],
   exports: [NgxPhotoEditorComponent],
-  entryComponents: [NgxPhotoEditorComponent]
+  entryComponents: []
 })
 export class NgxPhotoEditorModule {
 }
